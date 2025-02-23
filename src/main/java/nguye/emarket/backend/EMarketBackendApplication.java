665758cf49c7425e448b06fddd32b6730f40c8d3
nguyepham <nguyenminhpham24@gmail.com>
2025-02-23 10:33:21 +0700

    Initialize and configure OpenAPI generator

---
+++ src/main/java/nguye/emarket/backend/EMarketBackendApplication.java
@@ -0,0 +1,13 @@
+package nguye.emarket.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EMarketBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EMarketBackendApplication.class, args);
+    }
+
+}
